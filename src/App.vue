3c489f4edcd3d8385f00f928829a8a1984d9fddd
liangjinyong <liangjy881@gmail.com>
2025-01-30 11:09:26 +0900

    feat: add h1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <template>
-  <div class="box">
-    <h1>我是App</h1>
+  <div>
+    <h1>我新增一个标题</h1>
   </div>
 </template>
 
-<script setup lang="ts">
-for (let i = 0; i < 100; i++) 
+<script setup lang="ts"></script>
 
-{
-  console.log(i);
-}
-</script>
-
-<style scoped>
-.box {
-  width: 600px;
-  height: 400px;
-  background: black;
-  h1 {
-    color: white;
-  }
-}
-</style>
+<style scoped></style>
