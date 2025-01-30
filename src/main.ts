f2e10fe84d6a8fec9fe5274c53a07cdb10bd3fae
liangjinyong <liangjy881@gmail.com>
2025-01-30 10:46:06 +0900

    提交代码

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-const fun = () => 
-  
-  
-{
-  console.log("这是没有格式化的代码");
+const fun = () => {
+  console.log('这是没有格式化的代码')
 }
 
-fun();
+let str="我是祖国的老花骨朵";
+fun()
