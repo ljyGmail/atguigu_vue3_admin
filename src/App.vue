4335e3d4258340a5b4ecf565cb081955b1527772
liangjinyong <liangjy881@gmail.com>
2025-01-30 11:35:12 +0900

    feat: 项目中集成element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <h1>我新增一个标题</h1>
+    <el-button type="primary" size="default" :icon="Plus">添加按钮</el-button>
+    <el-button type="success" size="small" :icon="Edit">编辑按钮</el-button>
+    <el-button type="danger" size="default" :icon="Delete">删除按钮</el-button>
+    <el-pagination
+      :page-sizes="[100, 200, 300, 400]"
+      layout="sizes, prev, pager, next"
+      :total="1000"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+</script>
 <style scoped></style>
