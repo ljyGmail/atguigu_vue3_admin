2b6b74bfe87f988891891ce31bcc6fb89d79c31c
liangjinyong <liangjy881@gmail.com>
2025-01-31 17:44:38 +0900

    feat: 顶部tabbar组件静态的搭建与拆分

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -7,10 +7,10 @@
         v-if="!item.meta.hidden"
         @click="goRoute"
       >
+        <el-icon>
+          <component :is="item.meta.icon"></component>
+        </el-icon>
         <template #title>
-          <el-icon>
-            <component :is="item.meta.icon"></component>
-          </el-icon>
           <span>{{ item.meta.title }}</span>
         </template>
       </el-menu-item>
---
+++ src/layout/tabbar/breadcrumb/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 顶部左侧的静态 -->
+  <el-icon style="margin-right: 10px">
+    <Expand></Expand>
+  </el-icon>
+  <!-- 左侧的面包屑 -->
+  <el-breadcrumb separator-icon="ArrowRight">
+    <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
